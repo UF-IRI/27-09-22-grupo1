cf55f0a8f60b73e0727194887d25832381ad6f49
Luis Sandoval <luisandovalhorna@gmail.com>
2022-09-27 18:04:04 -0300

    tarea finalizada

--- libreria/example.h
+++ libreria/example.h
@@ -1,9 +1,15 @@
 #pragma once
+#include<math.h>
+#include<cmath>
 
 namespace foobar
 {
     struct Example
     {
         int getValue() const;
+        float suma(float, float);
+        float hipotenusa(float cateto1, float cateto2);
     };
+
 }
+float suma(float, float);
--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,16 @@
 #include <cstdlib>
-#include <iostream>
 
 #include "example.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char** argv)
 {
     foobar::Example example{};
-    std::cout << "Example.getValue() => " << example.getValue() << std::endl;
-
+    cout << "Example.getValue() => " << example.getValue() << std::endl;
+    cout << "hola mundo" << endl;
+    float resultado = 0;
+    resultado = suma(3, 5);
+    cout << "el resultado es: " << resultado << endl;
     return EXIT_SUCCESS;
 }
